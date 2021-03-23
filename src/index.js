09421fe7b1c6674aa7c3f0821b4659b5a410168e
Antonio <aguzmanv09@gmail.com>
2021-03-23 08:30:11 -0600

    Start the practices

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Function to add two variables
+ * @param {NUMBER} a
+ * @param {NUMBER} b
+ * @returns {NUMBER}
+ */
+const sum = (a, b) => {
+  return a + b;
+}
+
+console.log(sum(1,10)); //output 11
+console.log(sum(10,30)); //output 40
